2f9b8030bf714aaeaffd1a772ce8b4245495d1ac
Felix Krumme <felix.c.krumme@gmail.com>
2025-07-17 18:41:21 +0200

    Coded the entire logic of the application. Edited the README to explain the application.

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # Etch-a-Sketch
-My version of the Etch-a-Sketch-exercise of [TheOdinProject](https://www.theodinproject.com).
+My version of the Etch-a-Sketch exercise from [TheOdinProject](https://www.theodinproject.com).
+
+This app lets you fill up to 10,000 squares individually with randomly generated colors.
+
+Use the **Change Size** button to set the canvas width and height (up to 100 squares per side).
+
+Use the **Reset Canvas** button to clear the canvas and start fresh.
+
+When you hover over a square multiple times, it gradually gets darker, eventually *turning black* after 10 hovers.
--- script.js
+++ script.js
@@ -1,1 +1,50 @@
-console.log("test")
+const container = document.querySelector(".container");
+const sizeButton = document.querySelector(".sizeButton");
+const resetButton = document.querySelector(".resetButton");
+let value = 16
+
+generateGrid(value);
+function generateGrid(size) {
+    for (let i = 0; i < size*size; i++) {
+        const div = document.createElement("div");
+        div.classList.add("div");
+        div.style.width = `calc(100%/${size})`;
+        container.appendChild(div);
+        const darkbg = document.createElement("div")
+        darkbg.classList.add("darkbg")
+        div.appendChild(darkbg)
+        div.onmouseover = () => {
+            div.style.backgroundColor = randomRGB();
+            current_opacity = parseFloat(darkbg.style.opacity) || 0;
+            darkbg.style.opacity = Math.min((current_opacity + 0.1), 1).toString()
+            console.log(darkbg.style.opacity)
+            // if (darkbg.style.opacity <= "100%") {
+            // }
+        };
+    };
+}
+function resetGrid() {
+    container.innerHTML = "";
+    generateGrid(value)
+}
+
+sizeButton.onclick = () => {
+    value = prompt("Please enter the desired number of squares per side");
+    if (value > 100) {
+        value = prompt("The maximum canvas size is 100x100 squares. \nPlease enter a smaller size.");
+    } else {
+        resetGrid();
+        generateGrid(value);
+    }
+};
+
+resetButton.onclick = () => resetGrid();
+
+
+function randomRGBValue() {
+    return Math.floor(Math.random() * 256)
+}
+
+function randomRGB() {
+    return `rgb(${randomRGBValue()}, ${randomRGBValue()}, ${randomRGBValue()})`
+}
